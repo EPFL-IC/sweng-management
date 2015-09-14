e9b36f55146db91741d97ec893510fbc9a83d6d6
Stefan Bucur <sbucur@google.com>
2015-09-13 19:17:01 -0700

    Added a setup script that prepares a Python virtualenv.

---
+++ setup_venv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright 2015 Google Inc.
+# Author: Stefan Bucur (sbucur@google.com)
+
+set -e
+
+# Temporarily download Virtualenv's source code
+mkdir virtualenv
+curl https://pypi.python.org/packages/source/v/virtualenv/virtualenv-13.1.2.tar.gz | tar -xz -C virtualenv --strip-components=1
+
+# Create our virtual environment, called "venv"
+python virtualenv/virtualenv.py venv
+
+# Remove Virtualenv's source code
+rm -rf virtualenv
+
+# Setup "venv"
+source venv/bin/activate
+pip install -U pip
+pip install -r requirements.txt
+deactivate
